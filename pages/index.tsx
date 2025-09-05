76a31f90bf8208d55159a9e814ff7b4fa7828205
growas <storebundle2@gmail.com>
2025-09-05 13:05:09 +0000

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Data4gh</h1>
+      <p>Project running locally and ready for deployment.</p>
+    </div>
+  );
+}
